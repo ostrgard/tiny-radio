95f84f2a0abae34de2350da0d77e872e9d689fe4
Søren Østergaard <soestergaards@gmail.com>
2020-10-20 20:24:30 +0200

    eslint/prettier/refactor stuffs

--- RadioPlayer.js
+++ RadioPlayer.js
@@ -1,43 +1,36 @@
-const icy = require('icy');
-const lame = require('@suldashi/lame');
-const Speaker = require('speaker');
+const icy = require('icy')
+const lame = require('@suldashi/lame')
+const Speaker = require('speaker')
 
 class RadioPlayer {
   constructor(url) {
     this.playing = false
     this.url = url
-    this.currentStation = undefined
   }
 
   stop() {
     this.playing = false
     this.currentStation = undefined
+    this.currentSong = undefined
     if (this.stopCurrentRadio) this.stopCurrentRadio()
   }
 
   play() {
-    icy.get(this.url, res => {
-      this.playing = true
+    icy.get(this.url, (res) => {
+      res.pipe(new lame.Decoder()).pipe(new Speaker())
+
+      res.on('metadata', (metadata) => {
+        const parsed = icy.parse(metadata)
+        this.currentSong = parsed.StreamTitle
+      })
+
       this.currentStation = res.headers['icy-name']
-
-      res.on('metadata', metadata => {
-        var parsed = icy.parse(metadata);
-        console.error('Now playing', parsed.StreamTitle);
-      });
-
-      res.pipe(new lame.Decoder())
-         .pipe(new Speaker());
-
-      if (!this.playing) {
+      this.playing = true
+      this.stopCurrentRadio = () => {
         res.destroy()
-        this.stop()
-      } else {
-        this.stopCurrentRadio = () => {
-          res.destroy()
-          this.stopCurrentRadio = undefined
-        }
+        this.stopCurrentRadio = undefined
       }
-    });
+    })
   }
 }
 
--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const RadioPlayer = require('./RadioPlayer')
 
 const radioPlayer = new RadioPlayer('http://live-icy.gss.dr.dk/A/A29H.mp3')
 radioPlayer.play()
-console.log(radioPlayer.currentStation)
-setTimeout(() => radioPlayer.stop(), 1000)
+
+setTimeout(() => {
+  console.log(radioPlayer.currentStation, radioPlayer.currentSong)
+  radioPlayer.stop()
+}, 1000)
